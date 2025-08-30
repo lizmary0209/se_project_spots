c97dd133e1e1fd0f87b4c3dfe54f9ee621ca1064
Lizmary Chardon <lizmary0209@gmail.com>
2025-08-30 01:05:12 -0400

    fix:json webpack

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,9 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    main: "./src/pages/index.js",
-  },
+  entry: "./src/pages/index.js", // your main JS file
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -16,46 +14,45 @@ module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   stats: "errors-only",
+
   devServer: {
-    static: path.resolve(__dirname, "./dist"),
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
     compress: true,
     port: 8080,
     open: true,
     liveReload: true,
     hot: false,
   },
+
   target: ["web", "es5"],
+
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
-        exclude: "/node_modules/",
+        exclude: /node_modules/,
+        use: "babel-loader",
       },
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          "postcss-loader",
-        ],
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
   ],
 };
